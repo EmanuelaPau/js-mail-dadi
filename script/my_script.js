55a8832f607ce6212af1969ab4a53f5cd0c9c036
EmanuelaPau <uapame@gmail.com>
2023-05-17 14:40:30 +0200

    Analizing the dice game structure.

---
+++ script/my_script.js
@@ -0,0 +1,23 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Teniamo presente che per il gioco dei dadi non serviranno gli array, ma per l'esercizio delle mail....
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+// Tenete presente che non è permesso utilizzare funzioni built-in degli array come Array.includes() e Array.indexOf(), pena l'annullamento dell'esercizio.
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// si ma noi cosa vogliamo fare?
+// torniamo a scrivere in italiano
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+// 1.creo una variabile il cui valore e' un numero random da 1 a 6 
+// 2. creo una seconda variabile il cui valore e' un numero random da 1 a 6 
+// 3. stabilisco il vincitore:
+    // se il primo numero e' > del secondo vince il primo giocatore
+    // se il primo numero e' < del secondo vince il secondo giocatore
+    // se i numeri sono lo stesso > il risultato e' pari
