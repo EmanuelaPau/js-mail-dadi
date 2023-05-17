1befe76be55c5cc885f856e30240a3d9516380f4
EmanuelaPau <uapame@gmail.com>
2023-05-17 14:59:33 +0200

    Analizing the mail checker structure

--- script/my_script.js
+++ script/my_script.js
@@ -1,20 +1,9 @@
-// Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-// Teniamo presente che per il gioco dei dadi non serviranno gli array, ma per l'esercizio delle mail....
+
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 // Tenete presente che non è permesso utilizzare funzioni built-in degli array come Array.includes() e Array.indexOf(), pena l'annullamento dell'esercizio.
-// Consigli del giorno:
-// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// si ma noi cosa vogliamo fare?
-// torniamo a scrivere in italiano
-// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
-
+// -- DADI--
 // 1.creo una variabile il cui valore e' un numero random da 1 a 6 
 // 2. creo una seconda variabile il cui valore e' un numero random da 1 a 6 
 // 3. stabilisco il vincitore:
@@ -39,3 +28,14 @@ if (firstDice > secondDice) {
 } else {
     console.log("Draw!")
 }
+
+// -- MAIL -- 
+// 1. creo una lista di email a cui l'utente puo' accedere (array)
+// 2. creo un prompt per chiedere all'utente la sua email
+// 3. creo un ciclo for in cui:
+// definisco la lunghezza del ciclo in base al numero degli elementi dell'array 
+// prendo l' elemento del mio Array corrispondente al numero del ciclo e controllo che il suo valore sia uguale al valore del prompt
+// continuo fino alla fine degli elementi dell'array
+// 4. stabilisco se la mia email corrisponde o no
+
+
